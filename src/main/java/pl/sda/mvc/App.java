1f9c4d23447cf6e97f8f1a0d827e81452736352b
Joanna Kęprowska <a.keprowska@gmail.com>
2019-07-24 02:27:14 +0200

    zmienić np Tired na NOTTIRED ect

--- src/main/java/pl/sda/mvc/App.java
+++ src/main/java/pl/sda/mvc/App.java
@@ -7,6 +7,8 @@ import pl.sda.mvc.model.Health;
 import pl.sda.mvc.model.Nourishment;
 import pl.sda.mvc.view.GiantView;
 
+import java.util.ArrayList;
+
 /**
  * Hello world!
  *
@@ -15,17 +17,23 @@ public class App
 {
     public static void main( String[] args ) {
 
+        //
+
         GiantModel giantModel = new GiantModel(Health.HEALTH,
                 Fatigue.NOTTIRED,
                 Nourishment.HUNGRY);
 
         GiantView giantView = new GiantView();
 
-        GiantController controller = new GiantController(giantModel, giantView);
+        List<GiantModel> models = new ArrayList<>();
+        models.add(giantModel);
+
+
+        GiantController controller = new GiantController(models, giantView);
         controller.updateView();
-        controller.setHealth(Health.HEALTH);
+        controller.setHealth(Health.DEAD, 1);
         controller.updateView();
-        controller.setFatigue(Fatigue.TIRED);
-        controller.updateView();
+      //  controller.setFatigue(Fatigue.TIRED);
+       // controller.updateView();
     }
 }
